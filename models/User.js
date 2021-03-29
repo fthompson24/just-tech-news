6b4817168611dd77931b44fd7af42247815693ec
fthompson24 <fraserjthompson@gmail.com>
2021-03-28 20:32:35 -0500

    created Post model

--- models/User.js
+++ models/User.js
@@ -6,9 +6,9 @@ const sequelize = require('../config/connection');
 class User extends Model {
   // set up method to run on instance cata (per user) to check password
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+  return bcrypt.compareSync(loginPw, this.password);
   }
-}
+};
 
 // create fields/columns for User model
 User.init(
@@ -40,18 +40,18 @@ User.init(
     }
   },
   {
-    hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
-      async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      // set up beforeUpdate lifecycle "hook" functionality
-      async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      }
+  hooks: {
+    // set up beforeCreate lifecycle "hook" functionality
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
     },
+    // set up beforeUpdate lifecycle "hook" functionality
+    async beforeUpdate(updatedUserData) {
+      updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      return updatedUserData;
+    }
+  },
     sequelize,
     timestamps: false,
     freezeTableName: true,
