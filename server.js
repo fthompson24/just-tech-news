8ddeaf814f83348c20c7285e9aadba9429aa51e7
fthompson24 <fraserjthompson@gmail.com>
2021-04-02 12:40:43 -0500

    user can edit/delete posts

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
-const session = require('express-session');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-require('dotenv').config();
-
-
 const sess = {
-  secret: process.env.SESS_sec,
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
-  saveUninitialize: true,
+  saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
 
-
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
